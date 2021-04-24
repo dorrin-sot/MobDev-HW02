06d4b3ebf107eb48ff7b382dc126f09e2e6984b0
Peyman-hme <peyman.eb13@gmail.com>
2021-04-24 19:18:22 +0430

    User location implemented

--- app/src/main/java/com/mobdev/locationapp/MainActivity.java
+++ app/src/main/java/com/mobdev/locationapp/MainActivity.java
@@ -81,4 +81,5 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         MapFragment.mapView.onDestroy();
     }
+
 }
--- app/src/main/java/com/mobdev/locationapp/ui/map/MapFragment.java
+++ app/src/main/java/com/mobdev/locationapp/ui/map/MapFragment.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
@@ -16,39 +23,98 @@ import com.mapbox.mapboxsdk.maps.Style;
 import com.mobdev.locationapp.MainActivity;
 import com.mobdev.locationapp.R;
 
-public class MapFragment extends Fragment {
+import java.util.List;
+public class MapFragment extends Fragment implements
+        OnMapReadyCallback, PermissionsListener{
     public static MapView mapView;
+    private PermissionsManager permissionsManager;
+    private MapboxMap mapboxMap;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         Mapbox.getInstance(MainActivity.activity,getString(R.string.mapbox_access_token));
        View view = inflater.inflate(R.layout.fragment_map, container, false);
 
-       // setContentView(R.layout.activity_main);
+// Mapbox access token is configured here. This needs to be called either in your application
+// object or in the same activity which contains the mapview.
+        Mapbox.getInstance(MainActivity.activity, getString(R.string.mapbox_access_token));
 
-        mapView = (MapView) view.findViewById(R.id.mapView);
+// This contains the MapView in XML and needs to be called after the access token is configured.
+       // setContentView(R.layout.activity_location_component);
+
+        mapView = view.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(@NonNull MapboxMap mapboxMap) {
-
-                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
-                    @Override
-                    public void onStyleLoaded(@NonNull Style style) {
-
-                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments
-
-
-                    }
-                });
-
-            }
-        });
+        mapView.getMapAsync(this);
        // return inflater.inflate(R.layout.fragment_map, container, false);
         return view;
     }
+    @Override
+    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+        MapFragment.this.mapboxMap = mapboxMap;
+
+        mapboxMap.setStyle(Style.MAPBOX_STREETS,
+                new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+                        enableLocationComponent(style);
+                    }
+                });
+    }
+
+    @SuppressWarnings( {"MissingPermission"})
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+// Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(MainActivity.activity)) {
+
+// Get an instance of the component
+            LocationComponent locationComponent = mapboxMap.getLocationComponent();
+
+// Activate with options
+            locationComponent.activateLocationComponent(
+                    LocationComponentActivationOptions.builder(MainActivity.activity, loadedMapStyle).build());
+
+// Enable to make component visible
+            locationComponent.setLocationComponentEnabled(true);
+
+// Set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+
+// Set the component's render mode
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(MainActivity.activity);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(MainActivity.activity, R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableLocationComponent(style);
+                }
+            });
+        } else {
+            Toast.makeText(MainActivity.activity, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+           // finish();
+        }
+    }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         mapView.onDestroy();
     }
+
 }
