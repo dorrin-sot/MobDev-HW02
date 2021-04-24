4d8e699f34dc00775774ce84e0720a9360dd2539
Peyman-hme <peyman.eb13@gmail.com>
2021-04-24 17:34:49 +0430

    Map first implementation

--- app/src/main/java/com/mobdev/locationapp/MainActivity.java
+++ app/src/main/java/com/mobdev/locationapp/MainActivity.java
@@ -11,16 +11,19 @@ import androidx.room.Room;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.mobdev.locationapp.Model.LocationDB;
+import com.mobdev.locationapp.ui.map.MapFragment;
 
 import static com.mobdev.locationapp.Logger.d;
 
 public class MainActivity extends AppCompatActivity {
     static LocationDB db;
 
+    public static AppCompatActivity activity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activity=this;
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -35,12 +38,47 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-
+         MapFragment.mapView.onStart();
         if (db == null) {
             db = Room.databaseBuilder(getApplicationContext(),
                     LocationDB.class, "bookmark-database")
                     .build();
             d("Initialized database");
         }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        MapFragment.mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MapFragment.mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        MapFragment.mapView.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        MapFragment.mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        MapFragment.mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        MapFragment.mapView.onDestroy();
     }
 }
--- app/src/main/java/com/mobdev/locationapp/ui/map/MapFragment.java
+++ app/src/main/java/com/mobdev/locationapp/ui/map/MapFragment.java
@@ -8,12 +8,47 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mobdev.locationapp.MainActivity;
 import com.mobdev.locationapp.R;
 
 public class MapFragment extends Fragment {
-
+    public static MapView mapView;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        Mapbox.getInstance(MainActivity.activity,getString(R.string.mapbox_access_token));
+       View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+       // setContentView(R.layout.activity_main);
+
+        mapView = (MapView) view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+
+
+                    }
+                });
+
+            }
+        });
+       // return inflater.inflate(R.layout.fragment_map, container, false);
+        return view;
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mapView.onDestroy();
     }
 }
